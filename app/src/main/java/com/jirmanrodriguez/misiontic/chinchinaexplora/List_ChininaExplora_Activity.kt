8970a11b977b32ebfae4e2eae15eb08ca23c2eaf
Alexander Hernández <ja.hernandez211@gmail.com>
2021-11-20 17:44:54 -0500

    Activity chinchina

---
+++ app/src/main/java/com/jirmanrodriguez/misiontic/chinchinaexplora/List_ChininaExplora_Activity.kt
@@ -0,0 +1,11 @@
+package com.jirmanrodriguez.misiontic.chinchinaexplora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class List_ChininaExplora_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_chinina_explora)
+    }
+}
