04d6394a7b362dea9346b2071b8669dfd0abb28d
Alexander Hernández <ja.hernandez211@gmail.com>
2021-11-20 21:43:53 -0500

    recycler View

--- app/src/main/java/com/jirmanrodriguez/misiontic/chinchinaexplora/Sitiosturisticos.java
+++ app/src/main/java/com/jirmanrodriguez/misiontic/chinchinaexplora/Sitiosturisticos.java
@@ -3,7 +3,5 @@ package com.jirmanrodriguez.misiontic.chinchinaexplora;
 public class sitiosturisticos {
     val nombre: String,
     val descripcion: String,
-    val puntuacion: Float,
-    val ubicacion: String,
-    val temperatura: Float,
+    val puntuacion: String
 }
